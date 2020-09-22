81183265e1aeb66c0aaa42fecb453445476af503
Zero <zeroto521@gmail.com>
2020-09-22 22:46:45 +0800

    :white_check_mark: :bug: fix ci can't download python=3.9

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
 
     "Topic :: Software Development",
     "Topic :: Software Development :: Libraries",
