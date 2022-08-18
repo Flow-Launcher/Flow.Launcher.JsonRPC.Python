6cbf7b99085c220b204deda97f62fab89fa9af69
Zero <Zeroto521@gmail.com>
2022-08-18 14:21:39 +0800

    expose RPC

--- flowlauncher/__init__.py
+++ flowlauncher/__init__.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-
 from ._version import get_versions
-from .FlowLauncher import FlowLauncher  # noqa
-from .FlowLauncherAPI import FlowLauncherAPI  # noqa
+from .FlowLauncher import FlowLauncher  # noqa: F401
+from .FlowLauncherAPI import FlowLauncherAPI  # noqa: F401
+from .RPC import RPC
 
 __version__ = get_versions()["version"]
 del get_versions
